e4c76499a9beccd46289056f3c2063b389abe72b
Csicseri <88243565+Csicseri@users.noreply.github.com>
2021-08-06 20:24:24 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ var robotkepek = [
     "sophia.jpg",
     "robotallatok.jpg",
     "robot.jpg",
-    "tancol.jpg",
+    "tancol.png",
 ];
 var kepIndex = 0;
 var fo_kep = document.getElementById("fo-kep");
